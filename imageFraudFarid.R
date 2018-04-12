c250273e0409088249ea4a9603b8ad533e2940e9
robinyancey <reyancey@ucdavis.edu>
2018-04-12 15:52:40 -0700

    up to step 5 !

--- imageFraudFarid.R
+++ imageFraudFarid.R
@@ -19,8 +19,8 @@ height<-nrow(pic)
 approx <- matrix(nrow = height, ncol = width)
 boxside <- 8
 size <- (height*width)/boxside
-test <- matrix(nrow = size, ncol = 66)
-
+test <- matrix(nrow = size, ncol = (boxside^2) + 2)
+q <- 256
 print(system.time(
 
 for (i in 1:(width-boxside+1)){
@@ -30,17 +30,17 @@ for (i in 1:(width-boxside+1)){
   endh <- j+(boxside-1)
 
   pca <- prcomp(pic[j:endh,i:endw])
-  components <- 1:8
+  components <- 1:boxside
   features <- pca$rotation[,components]
   compact <- t(features) %*% t(pic[j:endh,i:endw])
   approx[j:endh,i:endw] <- t(features %*% compact)
   # prints approximated image
   # image(approx, col = grey(seq(0, 1, length = 256)))
-  test[(i+j-1),1:64] <- as.vector(round(rescale(approx[j:endh,i:endw], to = c(0, 256))))
+  test[(i+j-1),1:boxside^2] <- as.vector(round(rescale(approx[j:endh,i:endw], to = c(0, q))))
   # save location
-  test[(i+j-1),65:66] <- c(i,j)
+  test[(i+j-1),((boxside^2)+1):((boxside^2)+2)] <- c(i,j)
   }}
 
 ))
 # sort by all columns
-print(system.time(test <- test[do.call(order, lapply(1:64, function(i) test[,i])),]))
+print(system.time(test <- test[do.call(order, lapply(1:(boxside^2), function(i) test[,i])),]))
