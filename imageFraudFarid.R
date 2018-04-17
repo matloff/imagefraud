721c60891475ac1d26b2ecf398795722c7318a85
robinyancey <reyancey@ucdavis.edu>
2018-04-17 00:13:23 -0700

    array indexing corrected

--- imageFraudFarid.R
+++ imageFraudFarid.R
@@ -1,8 +1,10 @@
 library(jpeg)
 library(EBImage)
 library(scales)
+
 # test image
 pic <- Image(flip(readImage("/Users/robinyancey/desktop/cat.jpg")))
+
 
 #pca on black and white
 red.weigth   <- .2989; green.weigth <- .587; blue.weigth  <- 0.114
@@ -10,7 +12,7 @@ pic <- red.weigth * imageData(pic)[,,1] + green.weigth * imageData(pic)[,,2] + b
 #image(pic, col = grey(seq(0, 1, length = 256)))
 
 # pca on each color
-# pic <- pic[,,1]
+#pic <- pic[,,1]
 # g <- pic[,,2]
 # b <- pic[,,3]
 
@@ -18,29 +20,35 @@ width<-ncol(pic)
 height<-nrow(pic)
 approx <- matrix(nrow = height, ncol = width)
 boxside <- 8
-size <- (height*width)/boxside
+thresh <- 16
+
+size <- (width-boxside+1)*(height-boxside+1)
 test <- matrix(nrow = size, ncol = (boxside^2) + 2)
+Nd <- matrix(nrow = size, ncol = 1)
 q <- 256
+k <- 1
 print(system.time(
 
-for (i in 1:(width-boxside+1)){
-  for (j in 1:(height-boxside+1)){
-
-  endw <- i+(boxside-1)
-  endh <- j+(boxside-1)
-
-  pca <- prcomp(pic[j:endh,i:endw])
-  components <- 1:boxside
-  features <- pca$rotation[,components]
-  compact <- t(features) %*% t(pic[j:endh,i:endw])
-  approx[j:endh,i:endw] <- t(features %*% compact)
-  # prints approximated image
-  # image(approx, col = grey(seq(0, 1, length = 256)))
-  test[(i+j-1),1:boxside^2] <- as.vector(round(rescale(approx[j:endh,i:endw], to = c(0, q))))
-  # save location
-  test[(i+j-1),((boxside^2)+1):((boxside^2)+2)] <- c(i,j)
-  }}
-
+  for (i in 1:(width-boxside+1)){
+    for (j in 1:(height-boxside+1)){
+      
+      endw <- i+(boxside-1)
+      endh <- j+(boxside-1)
+      pca <- prcomp(pic[j:endh,i:endw])
+      components <- 1:boxside
+      features <- pca$rotation[,components]
+      compact <- t(features) %*% t(pic[j:endh,i:endw])
+      approx[j:endh,i:endw] <- t(features %*% compact)
+      # prints approximated image
+      # image(approx, col = grey(seq(0, 1, length = 256)))
+      test[k,1:boxside^2] <- as.vector(round(rescale(approx[j:endh,i:endw], to = c(0, q))))
+      # save location
+      test[k,((boxside^2)+1):((boxside^2)+2)] <- c(i,j)
+      k <- k + 1
+    }}
+  
 ))
+
 # sort by all columns
 print(system.time(test <- test[do.call(order, lapply(1:(boxside^2), function(i) test[,i])),]))
+#
