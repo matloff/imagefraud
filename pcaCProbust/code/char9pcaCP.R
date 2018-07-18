f42c180ba22fbf361a662226068c3717cf1e6a7d
robinyancey <reyancey@ucdavis.edu>
2018-07-17 17:39:03 -0700

    update parameters

--- pcaCProbust/code/char9pcaCP.R
+++ pcaCProbust/code/char9pcaCP.R
@@ -1,46 +1,43 @@
 library(EBImage)
 
-# NOTE: will parallelize soon
-# features input
-
 imageIn <- readImage("/Users/robinyancey/desktop/copied.jpg")
 
 display(imageIn)
 
-Nf <- 8 #20 #87 the program prints row/column pairs of offset frequencies greater than Nf 
+Nf <- 35 #20 #87 the program prints row/column pairs of offset frequencies greater than Nf 
 # adjust Nf up or down so that the # pairs printed  = # copied regions, larger copied regions should have higher Nf
-Nd <- 100  #280 # minimum offset distance of the matching block: increase as much as possible to remove any false
+Nd <- 150  #280 # minimum offset distance of the matching block: increase as much as possible to remove any false
 # positives (boxesnot in the copied region)
 
 # user choices
-dim3 <- 3 # 3 for color and 1 for b/w input image
+
 c <- 0 # color (0-255) of copied regions in output image
 boxside <- 32
 pfeatures <- 8
 # (will implement in parallel for this too very soon)
-par <- 2 # if 2,4,8, or 16 then image is split in chunks for parallel pca matrix computation, if 0 it runs in serial
+par <- 4 # if 2,4,8, or 16 then image is split in chunks for parallel pca matrix computation, if 0 it runs in serial
 # for 512x512 image:  seconds if par=,  seconds if par=0 
 # note1: parallel version requires partools package 
 # note2: higher # of parallel clusters could result in a false positive occuring in the splitting line (see test images)
-
-pcaCProbust<-function(imageIn,c=0,par=8,dim3=3,Nf=10,Nd=2,boxside=16,pfeatures=0){
+pcaCProbust<-function(imageIn,c=0,Nf=10,Nd=2,par=4,boxside=16,pfeatures=0){
   
   # note that images are read in differently (depending on function/package)
   width <- nrow(imageIn) 
   height<- ncol(imageIn)
+  dim <- dim(imageIn)
   
-  imageInCopy <-imageIn #we want to work witha b/w image
+  imageInCopy <-imageIn # to work with a b/w image
   
-  if (dim3 == 3){
+  if (dim[3] == 3){
     # standard way to convert to black and white
     red.weight<- .2989; green.weight <- .587; blue.weight <- 0.114
     imageIn <- red.weight * imageData(imageIn)[,,1] + green.weight * imageData(imageIn)[,,2] + blue.weight  * imageData(imageIn)[,,3]}
   
   
   # add a 3rd dimension to color on if b/w input image:
-  if (dim3 == 1){imageInCopy<-array(imageInCopy,dim=c(width,height,3))}
+  if (is.na(dim[3])){imageInCopy<-array(imageInCopy,dim=c(width,height,3))}
   
-  pcaMatrix <- function(imageIn){
+  charMatrix <- function(imageIn){
     require('scales')
     imageIn <-as.matrix(imageIn) # distribsplit changes it to dataframe (which is not acceptable by dvtt)
     width <- nrow(imageIn)
@@ -57,7 +54,6 @@ pcaCProbust<-function(imageIn,c=0,par=8,dim3=3,Nf=10,Nd=2,boxside=16,pfeatures=0
         pca <- prcomp(imageIn[i:endw,j:endh])
         features <- pca$rotation[,1]
         compact <- t(features) %*% t(imageIn[i:endw,j:endh])
-        #block <- round(rescale(t(features %*% compact), to = c(0, 255)))
         
         sumBlock <- boxside * boxside
         f1 <- sum(imageIn[i:endw,j:endh]) / (sumBlock)
@@ -96,27 +92,42 @@ pcaCProbust<-function(imageIn,c=0,par=8,dim3=3,Nf=10,Nd=2,boxside=16,pfeatures=0
     testpca
   } 
   
+  
   ### Parallel:
   if (par>0){
     require('partools') 
     cls <-makeCluster(par)
-    clusterExport(cls, varlist=c('dctMatrix', "boxside"), envir=environment())
+    clusterExport(cls, varlist=c('charMatrix',"pfeatures", "boxside","height"), envir=environment())
     distribsplit(cls, 'imageIn')
+    #new :)
+    rowseven <- round(width/length(cls))
+    imageIn2 <- imageIn[(rowseven+1):(rowseven+(boxside-1)),]
 
-    testpcaC <- clusterEvalQ(cls, testpcaC <- pcaMatrix(imageIn))
+    if (par > 2){
+      for (i in 2:(par-1)){
+        j <- rowseven*i+1
+        k <- rowseven*i+(boxside-1)
+        imageIn2<-rbind(imageIn2, imageIn[j:k,])}}
+
+    imageIn2 <- rbind(imageIn2, matrix(0, (boxside-1), dim(imageIn)[2]))
+    distribsplit(cls, 'imageIn2')
+    clusterEvalQ(cls, imageIn <- rbind(imageIn, imageIn2))
+    clusterEvalQ(cls, imageIn <- imageIn[apply(imageIn[,-1], 1, function(x) !all(x==0)),])
+
+    testpcaC <- clusterEvalQ(cls, testpcaC <- charMatrix(imageIn))
+    
     # need to correct i, j locations so add height/(cls[[n]]$rank-1) to i 
     for (i in 2:length(cls)){ 
-      testpcaC[[i]][,((boxside^2) + 1)] <- testpcaC[[i]][,((boxside^2) + 1)] + (i-1)*(height/length(cls)) 
+      testpcaC[[i]][,((boxside^2) + 10)] <- testpcaC[[i]][,((boxside^2) + 10)] + (i-1)*(height/length(cls)) 
     }
-    # combine all testpcaC chunks to make new large testpca
+    # combine all testdctC chunks to make new large testdct
     testpca<-do.call('rbind',testpcaC) 
   }
   
   
-  
   ### Serial:
   if (par==0){
-    testpca <- pcaMatrix(imageIn)}
+    testpca <- charMatrix(imageIn)}
   
   # rewrite size since was divided on cls (shorter since misses rows of overlapping boxes)
   size <- dim(testpca)[1]
@@ -141,11 +152,10 @@ pcaCProbust<-function(imageIn,c=0,par=8,dim3=3,Nf=10,Nd=2,boxside=16,pfeatures=0
     # only check first ten features of PCA (found by trial and error)
     if (all(testpca[i,1:9] == testpca[(i+1),1:9])){
      if (pfeatures == 0 || all(testpcaP[i,1:pfeatures] == testpcaP[(i+1),1:pfeatures])){
-#      if (all(testpcaP[i,1:3] == testpcaP[(i+1),1:3])){
         
         distancePair[numFound,1] <- abs(pcaLocations[i,1] - pcaLocations[(i+1),1]) # row offset
         distancePair[numFound,2] <- abs(pcaLocations[i,2] - pcaLocations[(i+1),2]) # column offset
-        if (sqrt(distancePair[numFound,1]^2+distancePair[numFound,1]^2)>Nd){ # absolute distance between matching blocks
+        if (sqrt(distancePair[numFound,1]^2+distancePair[numFound,1]^2) > Nd){ # absolute distance between matching blocks
           pairLoc1[numFound,] <- pcaLocations[i,] # record this location
           pairLoc2[numFound,] <- pcaLocations[(i+1),] # increment matrix counting offset frequencies:
           pairFrequencies[distancePair[numFound,1], distancePair[numFound,2]] <- pairFrequencies[distancePair[numFound,1], distancePair[numFound,2]] + 1
@@ -162,9 +172,9 @@ pcaCProbust<-function(imageIn,c=0,par=8,dim3=3,Nf=10,Nd=2,boxside=16,pfeatures=0
   
   for (i in 1:(numFound-1)){
     for (j in 1:nrow(freqPairs)){
-      if (distancePair[i,] == freqPairs[j,]){ # color matching boxes
-        imageInCopy[pairLoc1[i,1]:(pairLoc1[i,1]+boxside - 1), pairLoc1[i,2]:(pairLoc1[i,2]+boxside - 1),1:dim3] = c
-        imageInCopy[pairLoc2[i,1]:(pairLoc2[i,1]+boxside - 1), pairLoc2[i,2]:(pairLoc2[i,2]+boxside - 1),1:dim3] = c
+      if (round(distancePair[i,]) == freqPairs[j,]){ # color matching boxes
+        imageInCopy[round(pairLoc1[i,1]):(round(pairLoc1[i,1])+boxside - 1), round(pairLoc1[i,2]):(round(pairLoc1[i,2])+boxside - 1),1:3] = c
+        imageInCopy[round(pairLoc2[i,1]):(round(pairLoc2[i,1])+boxside - 1), round(pairLoc2[i,2]):(round(pairLoc2[i,2])+boxside - 1),1:3] = c
       }
     }
   }
@@ -173,6 +183,6 @@ pcaCProbust<-function(imageIn,c=0,par=8,dim3=3,Nf=10,Nd=2,boxside=16,pfeatures=0
 }
 
 
-print(system.time(imageInCopy<-pcaCProbust(imageIn,c,par,dim3,Nf,Nd,boxside,pfeatures)))
+print(system.time(imageInCopy<-pcaCProbust(imageIn,c,Nf,Nd,par,boxside,pfeatures)))
 # need to rerun this line to refresh image:
 display(imageInCopy)
