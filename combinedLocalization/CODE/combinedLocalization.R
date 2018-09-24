6556dfe92d29ebab80209726faa6503c3f89631f
robinyancey <reyancey@ucdavis.edu>
2018-09-23 19:39:32 -0700

    Create combinedLocalization.R

---
+++ combinedLocalization/CODE/combinedLocalization.R
@@ -0,0 +1,23 @@
+
+image<- '/Users/robinyancey/desktop/Tp_D_NRN_S_N_ani10210_ani10209_12373.jpg'
+im_out <- BAGlocalization(image)
+im_out2 <- ELAlocalization(image, Q=0.95, scale=25)
+
+im_out2 <- im_out2[1:nrow(im_out),1:ncol(im_out)]
+im_out3 <- im_out + im_out2
+display(im_out3)
+
+# this is just example input
+boxside <- 8
+thresh <- 0.3
+im_out4 <- rmBoxAvErrors(im_out3, boxside, thresh)
+
+# this is just example input
+boxside <- 16
+thresh <- 0.3
+im_out5 <- rmBoxAvErrors(im_out4, boxside, thresh)
+
+thresh <- 0.065
+im_out6 <- zeroOne(im_out5, thresh)
+
+display(im_out6)
